4a977652a4e5396e3829659344cc112c7fc6f48d
AbduxalilVoxidjonov <abduhalilvohidjonov@gmail.com>
2024-01-09 20:57:54 +0500

    Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,9 +49,9 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    implementation("io.reactivex.rxjava3:rxjava:3.1.2")
+    implementation("androidx.room:room-rxjava3:2.6.1")
     implementation("androidx.room:room-runtime:2.6.1")
-    implementation("io.reactivex.rxjava3:rxkotlin:3.0.1")
-    implementation("androidx.room:room-rxjava3:3.0.0")
     //noinspection KaptUsageInsteadOfKsp
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
--- app/src/main/java/com/example/rxkotlin/MainActivity.kt
+++ app/src/main/java/com/example/rxkotlin/MainActivity.kt
@@ -1,38 +1,83 @@
 package com.example.rxkotlin
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import com.example.rxkotlin.databinding.ActivityMainBinding
-import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.ObservableEmitter
-import io.reactivex.rxjava3.core.ObservableOnSubscribe
+import io.reactivex.rxjava3.core.BackpressureStrategy
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.core.FlowableEmitter
+import io.reactivex.rxjava3.core.FlowableOnSubscribe
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        createObservable().subscribe({
-            binding.textV.text = it
-        }, {
-            binding.textV.text = it.message
-        })
+        val fastflowable = Flowable.interval(100, TimeUnit.MILLISECONDS)
+            .onBackpressureDrop{
+                println("Dropped $it")
+            }
+        val createflowable = createFlowable()
+        createflowable
+            .buffer(5)
+            .subscribe{
+            binding.textV.text=it.toString()
+        }
+
+
+
+//        val b = createObservable()
+//            .debounce(2000,TimeUnit.SECONDS)
+//            .subscribeWith(object : Observer<String> {
+//                override fun onSubscribe(d: Disposable) {
+//                    TODO("Not yet implemented")
+//                }
+//
+//                override fun onError(e: Throwable) {
+//                    TODO("Not yet implemented")
+//                }
+//
+//                override fun onComplete() {
+//                    TODO("Not yet implemented")
+//                }
+//
+//                override fun onNext(t: String) {
+//                    TODO("Not yet implemented")
+//                }
+//            })
+//
+//        binding.btn.setOnClickListener {
+//            b.onComplete()
+//        }
+//
 
     }
 
-    fun createObservable(): Observable<String> {
-        return Observable.create(object : ObservableOnSubscribe<String> {
-            override fun subscribe(emitter: ObservableEmitter<String>) {
+//    fun createObservable(): Observable<String> {
+//        return Observable.create(object : ObservableOnSubscribe<String> {
+//            override fun subscribe(emitter: ObservableEmitter<String>) {
+//                binding.editTx.addTextChangedListener {
+//                    emitter.onNext(it.toString())
+//                }
+//            }
+//
+//        })
+//    }
+    fun createFlowable(): Flowable<String> {
+        return Flowable.create(object : FlowableOnSubscribe<String> {
+            override fun subscribe(emitter: FlowableEmitter<String>) {
                 binding.editTx.addTextChangedListener {
                     emitter.onNext(it.toString())
                 }
             }
 
-        })
+        }, BackpressureStrategy.BUFFER)
     }
 }
